88f16f3ce132378ba2fa873509b3f2bbd3470b2b
gour6380 <gour6380@gmail.com>
2016-12-08 18:22:02 +0530

    Like Added

---
+++ app/src/main/java/biti/sulitous/com/blog/BlogSingleActivity.java
@@ -0,0 +1,13 @@
+package biti.sulitous.com.blog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlogSingleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_single);
+    }
+}
