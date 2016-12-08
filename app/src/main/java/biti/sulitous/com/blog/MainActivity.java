68edf4ae03d0fece110faf9496c7c373d56550cc
gour6380 <gour6380@gmail.com>
2016-12-08 15:47:15 +0530

    Login added

--- app/src/main/java/biti/sulitous/com/blog/MainActivity.java
+++ app/src/main/java/biti/sulitous/com/blog/MainActivity.java
@@ -86,24 +86,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void checkUserExist() {
-        final String user_id = mAuth.getCurrentUser().getUid();
+        if (mAuth.getCurrentUser() != null) {
+            final String user_id = mAuth.getCurrentUser().getUid();
 
-        mDatabaseUsers.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
 
-                if (!dataSnapshot.hasChild(user_id)){
-                    Intent setupIntent = new Intent(MainActivity.this, SetupActivity.class);
-                    setupIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(setupIntent);
+                    if (!dataSnapshot.hasChild(user_id)) {
+                        Intent setupIntent = new Intent(MainActivity.this, SetupActivity.class);
+                        setupIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(setupIntent);
+                    }
                 }
-            }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
 
-            }
-        });
+                }
+            });
+        }
     }
 
     public static class BlogViewHolder extends RecyclerView.ViewHolder{
